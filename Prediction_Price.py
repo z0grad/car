198079b75bcd7a904dcf4cadacbf97ff339218d5
Zero Grad <112247214+z0grad@users.noreply.github.com>
2023-09-15 08:17:18 +0300

    Update Prediction_Price.py

--- Prediction_Price.py
+++ Prediction_Price.py
@@ -12,10 +12,9 @@ st.title('Car Price Prediction\n')
 df = pd.read_csv('Car_clean.csv')
 
 # Load Preprocessor
-# preprocessor = pd.read_pickle('preprocessor.pkl')
-# model = pd.read_pickle('model.pkl')
-preprocessor = pickle.load(open('preprocessor.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
+preprocessor = pd.read_pickle('preprocessor.pkl')
+model = pd.read_pickle('model.pkl')
+
 
 # Input Data
 
